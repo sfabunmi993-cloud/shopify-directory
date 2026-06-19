7ceb99eb2161e096ad8079a3a7911b2d65a2046f
Shopify invoice <fabunmi.net@gmail.com>
2026-06-19 03:39:28 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ import Messages from '@/pages/Messages.jsx';
 import About from '@/pages/About';
 import Contact from '@/pages/Contact';
 import SupportChat from '@/components/SupportChat';
+import AdPopup from '@/components/AdPopup';
 
 
 const AuthenticatedApp = () => {
@@ -84,6 +85,7 @@ function App() {
         </Router>
         <Toaster />
         <SupportChat />
+        <AdPopup />
       </QueryClientProvider>
     </AuthProvider>
   )
