83a80fbdabf705070507684a7756a15904635efc
Shopify invoice <fabunmi.net@gmail.com>
2026-07-06 14:45:05 +0000

    Visual edits

--- src/pages/PartnerDetail.jsx
+++ src/pages/PartnerDetail.jsx
@@ -445,7 +445,7 @@ export default function PartnerDetail() {
         <div className="space-y-6">
 
           {/* About */}
-          <div className="my-64 px-48">
+          <div className="px-5">
             <h2 className="font-heading text-xl font-bold text-foreground mb-4">About</h2>
 
             {partner.description &&
