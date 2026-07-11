834516faa688d83ce6bd3bac6699a97202428429
Shopify invoice <fabunmi.net@gmail.com>
2026-07-11 20:17:44 +0000

    File changes

--- src/components/partner/ContactModal.jsx
+++ src/components/partner/ContactModal.jsx
@@ -125,7 +125,7 @@ export default function ContactModal({ partner, isOpen, onClose, mode = 'inquiry
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="max-w-lg">
+      <DialogContent className="max-w-md">
         <DialogHeader>
           <DialogTitle>Contact {partner.name}</DialogTitle>
           <DialogDescription>
