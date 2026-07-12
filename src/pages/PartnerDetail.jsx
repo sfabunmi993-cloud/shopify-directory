9ce3cb383682a22cb95ffda9394669f13b346315
Shopify invoice <fabunmi.net@gmail.com>
2026-07-12 18:33:22 +0000

    Visual edits

--- src/pages/PartnerDetail.jsx
+++ src/pages/PartnerDetail.jsx
@@ -425,7 +425,7 @@ export default function PartnerDetail() {
             {partner.completed_projects > 0 &&
             <div>
                 <p className="text-primary font-semibold text-xs uppercase tracking-wide">Partner Since 2014</p>
-                <p className="text-muted-foreground mt-0.5">{partner.completed_projects}</p>
+                <p className="text-muted-foreground mt-0.5 hidden">{partner.completed_projects}</p>
               </div>
             }
           </div>
