445e1e7082ed600a912abc1808b520f4dabe9dac
Shopify invoice <fabunmi.net@gmail.com>
2026-08-07 10:26:45 +0000

    Visual edits

--- src/components/partner/PurchasePremiumModal.jsx
+++ src/components/partner/PurchasePremiumModal.jsx
@@ -97,7 +97,7 @@ export default function PurchasePremiumModal({ partner, isOpen, onClose, user })
         <DialogHeader>
           <DialogTitle className="flex items-center gap-2">Purchase Premium Badge
 opay; 7031665045
-</DialogTitle>
+name;</DialogTitle>
         </DialogHeader>
 
         {done ?
