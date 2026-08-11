d167085dd238eb0686b15696742054153d762f31
Shopify invoice <fabunmi.net@gmail.com>
2026-08-11 12:56:58 +0000

    Visual edits

--- src/components/SupportChat.jsx
+++ src/components/SupportChat.jsx
@@ -91,7 +91,7 @@ export default function SupportChat() {
       {!isOpen &&
       <Button
         onClick={() => setIsOpen(true)}
-        className="fixed bottom-6 right-6 h-14 w-14 rounded-full shadow-lg z-50"
+        className="fixed bottom-6 right-6 h-14 w-14 rounded-full shadow-lg z-50 hidden"
         size="icon">
         
           <MessageCircle className="w-6 h-6" />
